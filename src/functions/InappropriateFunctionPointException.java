05aa574dabcfca9a1eb40db154db3bdd5cc03e31
Никита Носов <nikinosov2002@mail.ru>
2021-09-29 11:44:44 +0400

    Добавил два класса исключений

---
+++ src/functions/InappropriateFunctionPointException.java
@@ -0,0 +1,6 @@
+package functions;
+
+public class InappropriateFunctionPointException extends Exception{
+    /* исключение, выбрасываемое при попытке добавления или изменения точки функции несоответствующим образом
+    */
+}
